4377b201e59a4c78012cea47c1c8db8d69625c07
loiola0 <victorloiola@alu.ufc.br>
2021-06-04 19:18:46 -0300

    Preparing sales search navigation views

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -48,7 +48,7 @@ namespace SalesWebMvc.Services
                 _context.Seller.Remove(obj);
                 await _context.SaveChangesAsync();
             }
-            catch(DbUpdateException e){
+            catch(DbUpdateException){
                 throw new IntegrityException("Can't delete seller because he/she has sales!");
             }
 
